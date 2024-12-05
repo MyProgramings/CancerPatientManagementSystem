b8d97a509232871aa83a7b1fc55492a7fbc42bfa
MyProgramings <reader1999a@gmail.com>
2024-12-05 14:34:23 +0300

    done add dark mode

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-400 leading-tight">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
-    <div>
+    <div class="dark:bg-gray-800">
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
